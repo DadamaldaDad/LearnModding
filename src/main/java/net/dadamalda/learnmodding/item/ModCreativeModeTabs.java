a470378df2cffbf1eadb6542986c9cac17c2a106
Dadamalda <dadamaldac@gmail.com>
2024-06-06 12:20:26 +0200

    Add items: - Ruby - Raw Ruby

---
+++ src/main/java/net/dadamalda/learnmodding/item/ModCreativeModeTabs.java
@@ -0,0 +1,28 @@
+package net.dadamalda.learnmodding.item;
+
+import net.dadamalda.learnmodding.LearnModding;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, LearnModding.MODID);
+
+    public static final RegistryObject<CreativeModeTab> LEARN_MODDING_TAB = CREATIVE_MODE_TABS.register("learn_modding_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.RUBY.get()))
+                    .title(Component.translatable("creativetab.learn_modding_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.RUBY.get());
+                        output.accept(ModItems.RAW_RUBY.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
