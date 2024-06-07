79bd6c5c3aa66853b49a48a2aea65254c9397205
Dadamalda <dadamaldac@gmail.com>
2024-06-07 18:26:06 +0200

    Added ruby staff tooltip, now only works on monsters

--- src/main/java/net/dadamalda/learnmodding/item/custom/RubyStaffItem.java
+++ src/main/java/net/dadamalda/learnmodding/item/custom/RubyStaffItem.java
@@ -1,14 +1,19 @@
 package net.dadamalda.learnmodding.item.custom;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSources;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -23,8 +28,8 @@ public class RubyStaffItem extends Item {
             Level level = pContext.getLevel();
             Player player = pContext.getPlayer();
             Vec3 clickPosition = pContext.getClickLocation();
-            List<Mob> mobs = level.getEntitiesOfClass(Mob.class, AABB.ofSize(clickPosition, 8.0d, 4.0d, 8.0d));
-            for (Mob mob : mobs) {
+            List<Monster> mobs = level.getEntitiesOfClass(Monster.class, AABB.ofSize(clickPosition, 8.0d, 4.0d, 8.0d));
+            for (Monster mob : mobs) {
                 if(mob.getId() != player.getId()) {
                     mob.hurt(new DamageSources(level.registryAccess()).magic(), 5.0f);
                 }
@@ -35,5 +40,11 @@ public class RubyStaffItem extends Item {
         }
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("item.learn_modding.ruby_staff.tooltip"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 }
